70466d477c9ca01795eb5e1ee11a4700728274f5
MudkipZ10 <nabil291202@gmail.com>
2025-03-20 19:55:26 +0800

    revert. Change this to your local path haiyaa

--- sql databases/update_comic_images.sql
+++ sql databases/update_comic_images.sql
@@ -3,21 +3,21 @@ SET GLOBAL secure_file_priv = '';
 
 -- Update One Piece image
 UPDATE comic 
-SET comic_art = LOAD_FILE('../images/one_piece.webp')
+SET comic_art = LOAD_FILE('C:/wamp64/www/ESD/ESD-Weebs/images/one_piece.webp')
 WHERE comic_name = 'One Piece';
 
 SELECT ROW_COUNT() as rows_updated, 'One Piece' as comic;
 
 -- Update Attack on Titan image
 UPDATE comic 
-SET comic_art = LOAD_FILE('../images/attack_on_titan.webp')
+SET comic_art = LOAD_FILE('C:/wamp64/www/ESD/ESD-Weebs/images/attack_on_titan.webp')
 WHERE comic_name = 'Attack on Titan';
 
 SELECT ROW_COUNT() as rows_updated, 'Attack on Titan' as comic;
 
 -- Update Demon Slayer image
 UPDATE comic 
-SET comic_art = LOAD_FILE('../images/demon_slayer.webp')
+SET comic_art = LOAD_FILE('C:/wamp64/www/ESD/ESD-Weebs/images/demon_slayer.webp')
 WHERE comic_name = 'Demon Slayer';
 
 SELECT ROW_COUNT() as rows_updated, 'Demon Slayer' as comic;
